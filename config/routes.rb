49b3dc85c45f4a2f9924dfad84e9919c07a34585
James Harris <computersherpa@gmail.com>
2022-07-04 17:09:18 -0500

    Follow https://hibbard.eu/rails-react-crud-app/

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'site/index'
+  root to: 'site#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
