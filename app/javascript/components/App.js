bc5ba7e206f75a344e17007b67e433158f1e9eff
James Harris <computersherpa@gmail.com>
2022-07-04 20:51:00 -0500

    Add stylesheet, cork image, week of sticky notes

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
-const HelloMessage = ({ name }) => <h1>Hello, {name}!</h1>;
 
 import Sticky from './Sticky';
 
+const yellowStickySettings = {
+	size: 250,
+	color: '#FDFFAA',
+}
+
+const weekOfStickies = [
+	<Sticky size={200} color="#FDFFAA" boldHeader="Sunday,"    mediumHeader="July 3" key={1} />,
+	<Sticky size={200} color="#FDFFAA" boldHeader="Monday,"    mediumHeader="July 4" key={2} />,
+	<Sticky size={200} color="#FDFFAA" boldHeader="Tuesday,"   mediumHeader="July 5" key={3} />,
+	<Sticky size={200} color="#FDFFAA" boldHeader="Wednesday," mediumHeader="July 6" key={4} />,
+	<Sticky size={200} color="#FDFFAA" boldHeader="Thursday,"  mediumHeader="July 7" key={5} />,
+	<Sticky size={200} color="#FDFFAA" boldHeader="Friday,"    mediumHeader="July 8" key={6} />,
+	<Sticky size={200} color="#FDFFAA" boldHeader="Saturday,"  mediumHeader="July 9" key={7} />
+]
+
 export default ({ props }) => {
-	return <Sticky size={350} color="#FDFFAA" boldHeader="Monday," mediumHeader="June 4">Test</Sticky>
+	return <div className='overhead'>
+		{weekOfStickies}
+	</div>
 }
