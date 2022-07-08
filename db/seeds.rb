b73061865e736adbb8f4ff5e4d46483bf69a2e63
James Harris <computersherpa@gmail.com>
2022-07-08 16:56:01 -0500

    Create Task model and sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+p "Destroying old tasks"
+Task.destroy_all
+p "Creating new tasks"
+
+# Sunday tasks
+Task.create(completed: true, name: "Find some jobs on jobs.ie", date: "December 18 2022")
+Task.create(completed: false, name: "Find some jobs on irishjobs.ie", date: "December 18 2022")
+Task.create(completed: false, name: "Apply to Terraso", date: "December 18 2022")
+
+# Monday tasks
+Task.create(completed: false, name: "Apply to Sana", date: "December 19 2022")
+Task.create(completed: true, name: "Send resume to Caleb Runyon of GT Independence", date: "December 19 2022")
+Task.create(completed: false, name: "Talk to Matt Vasquez and Caitlin Studley", date: "December 19 2022")
+
+# Tuesday tasks
+Task.create(completed: false, name: "Email Chris Davidson with my resume, skill areas, growth areas", date: "December 20 2022")
+Task.create(completed: true, name: "Message Anebi on Slack", date: "December 20 2022")
+Task.create(completed: false, name: "Prep resume for UI Front End Developer at Apple and send to Nanette Weddell on LinkedIn", date: "December 20 2022")
+
+# Wednesday tasks
+Task.create(completed: true, name: "Order light sockets for Holly", date: "December 20 2022")
+Task.create(completed: false, name: "Sign up for makerspace forums", date: "December 20 2022")
+Task.create(completed: false, name: "On Urithuru, connect Hyper Backup to Backblaze", date: "December 20 2022")
+
+p "Done."
